9abb3ed8b4b1ba738bdb868ee2af7e0685277fc4
James Kuang <james.kuang.13@gmail.com>
2021-09-22 21:26:05 -0400

    Working on a bunch of visualizations

---
+++ Alt Fuel/FuelCorriderMap.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+
+corriders <- read_csv("Alt Fuel Corriders/Alternative_fuel_corridors.csv")
--- George Washington/test.py
+++ George Washington/test.py
@@ -1,1 +1,30 @@
-2 + 2
+import pandas as pd
+import seaborn as sns
+
+def heat_map(
+        df: pd.DataFrame,
+        color: str = 'lime',
+        fmt: str = '{:.0f}'
+) -> 'pd.io.formats.style.Styler':
+    """Adds color to a dataframe. Used earnestly to convey data in a more
+    readable way, or cynically to beguile management with cheap tricks.
+
+    :param df: Your lousy data.
+    :param color: The gaudiest color you can imagine.
+    :param fmt: Don't forget to format the cells.
+    :return: A beautiful dataframe in technicolor.
+    """
+    cmap = sns.light_palette(color, as_cmap=True)
+    return df.style.format(fmt).background_gradient(cmap=cmap, axis=None)
+
+
+# =====================================
+# example (renders in Jupyter Notebook)
+# =====================================
+
+df = pd.DataFrame({
+    'x': range(1, 4),
+    'y': range(4, 7),
+    'z': range(7, 10)
+}, index=['a', 'b', 'c'])
+print(heat_map(df))
