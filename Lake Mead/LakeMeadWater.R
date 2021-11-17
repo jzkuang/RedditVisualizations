c7dfab9e428d40ea619faa814b70e5be9e4eb0c9
James Kuang <james.kuang.13@gmail.com>
2021-11-17 15:47:25 -0500

    Update LakeMeadWater.R
    Creating the graph

--- Lake Mead/LakeMeadWater.R
+++ Lake Mead/LakeMeadWater.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rvest)
+library(lubridate)
 
 lcr_web <- read_html("https://www.usbr.gov/lc/region/g4000/hourly/mead-elv.html")
 lcr_web
@@ -14,4 +15,16 @@ water2011to2021 <- water_tables[[3]]
 water_levels <- waterTo2011 %>% 
   rbind(water2011to2021) %>% 
   mutate(JAN = as.numeric(JAN)) %>% 
-  pivot_longer(-c(Year), names_to = "Months", values_to = "Elevation")
+  pivot_longer(-c(Year), names_to = "Month", values_to = "Elevation") %>% 
+  mutate(date = ymd(paste(Year, Month, "1"))) 
+
+water_levels_year <- water_levels %>% 
+  group_by(Year) %>% 
+  summarise(
+    Avg_Elevation = mean(Elevation)
+  )
+
+ggplot(water_levels_year, aes(x = Year, y = Avg_Elevation)) +
+  geom_line() +
+  theme_classic() +
+  labs(y = "Average Yearly Water Elevation")
