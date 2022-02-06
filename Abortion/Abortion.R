f969f03fef3b82d82811c0df1037f8b325f35292
James Kuang <james.kuang.13@gmail.com>
2022-02-06 17:54:32 -0500

    Graphic Created

--- Abortion/Abortion.R
+++ Abortion/Abortion.R
@@ -6,3 +6,20 @@ abortion <- read_csv("Abortion/abortiondistances_countyxmonth_2009to2021.csv")
 abortion <- abortion %>% 
   separate(origin_county_name, c("county", "state"), sep = "\\(") 
 abortion["state"] <- gsub("\\)", "", abortion["state"])
+
+abortion %>% 
+  group_by(year, month) %>% 
+  summarise(
+    mean_tt = mean(tt)
+  ) %>% 
+  mutate(
+    date = make_date(year, month)
+  ) %>% 
+  ggplot(aes(x= date, y = mean_tt)) +
+  geom_line() +
+  theme_bw() +
+  labs(
+    x = "Date",
+    y = "Travel Time to the Nearest Abortion Facility (Minutes)"
+  )
+  
